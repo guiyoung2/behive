c21bd8fd7e51a4f20594ae950d5a3ef679b2e88e
guiyoung2 <guiyoung22@naver.com>
2023-01-06 15:36:23 +0900

    a
    a

--- js/main.js
+++ js/main.js
@@ -8,4 +8,27 @@ $(function () {
   menu.on("mouseleave", function () {
     $(this).find(".sub").stop().slideUp(300);
   });
+
+  $(document).ready(function () {
+    $(".m_menu > ul > li > a").click(function (event) {
+      event.preventDefault();
+      if ($(this).next().css("display") == "none") {
+        $(".sub:visible").slideUp();
+        $(".m_menu > ul > li > a").removeClass("active");
+        $(this).next().slideDown();
+        $(this).addClass("active");
+      } else {
+        $(this).removeClass("active");
+        $(this).next().slideUp();
+      }
+    });
+    $(".m_btn").on("click", function () {
+      $(".m_bg").show();
+      $(".m_menu_area").show();
+    });
+    $(".m_bg").on("click", function () {
+      $(".m_bg").hide();
+      $(".m_menu_area").hide();
+    });
+  });
 });
